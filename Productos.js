058a649e75aa0845b9dce2a2227474579b88a478
Diego130416 <l21141337@queretaro.tecnm.mx>
2024-09-22 18:14:29 -0600

    Add files via upload

---
+++ Productos.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+var datos = {};
+var misDatos = info => {
+    console.log(info);
+    datos.usuarios = info;
+    var html = "";
+    html+="<h2>Productos</h2>";
+    datos.usuarios.map(usuario => {
+		for(let propiedad of Object.keys(usuario)){
+			html+="<li>"+propiedad+": "+usuario[propiedad]+"</li>";
+		}
+		html+="<hr/>";
+    })
+    document.getElementById("resultados").innerHTML = html;
+}
